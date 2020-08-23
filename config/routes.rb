bf770434ac17f75b09bdbea864d00bce6272b10c
Ubuntu <ubuntu@ip-172-31-12-74.ap-northeast-1.compute.internal>
2020-08-23 02:58:20 +0000

    logout and current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
   root'static_pages#home'
+  
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup',    to: 'users#new'
+  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
