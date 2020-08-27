b21ea990246d14f6645fd71d66dc6cc6e24a1563
Ubuntu <ubuntu@ip-172-31-12-74.ap-northeast-1.compute.internal>
2020-08-27 04:53:25 +0000

    link_to

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if logged_in?
+    @music_names = ["stand out fit in","wasted nights","未完成行進曲","じぶんrock"] 
   end
 end
