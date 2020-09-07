80fa9ebede2b2d80a1a9cfdf727b38c28daa5817
Ubuntu <ubuntu@ip-172-31-12-74.ap-northeast-1.compute.internal>
2020-09-07 12:38:50 +0000

    割と見栄えを整えました

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,16 @@ class StaticPagesController < ApplicationController
     end
     
   end
+
+   
+   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user
+    else
+      flash.now[:danger] = "emailもしくはpasswordが間違っています"
+      render "new"
+    end
+   end
 end
